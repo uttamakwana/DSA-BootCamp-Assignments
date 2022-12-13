2bf497e2f5478a7097c1f54f5ce0f7c815c69ed3
Uttam Makwana <uttamakwana4503@gmail.com>
2022-12-13 11:39:31 +0530

    Created a Simple Hello World Printing Program to start Java

---
+++ 01.Flow-Of-Program/Main.java
@@ -0,0 +1,6 @@
+public class Main{
+    public static void main(String[] args){
+        // Just creating simple program and understading how Java code works.
+        System.out.println("Hello World!");
+    }
+}
