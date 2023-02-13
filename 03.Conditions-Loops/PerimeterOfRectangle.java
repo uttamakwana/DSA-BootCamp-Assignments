0fdedae4b41be8f514d4154f6e43a08e6d912e56
Uttam Makwana <uttamakwana4503@gmail.com>
2023-02-13 17:22:31 +0530

    Perimeter of Rectangle

---
+++ 03.Conditions-Loops/PerimeterOfRectangle.java
@@ -0,0 +1,8 @@
+public class PerimeterOfRectangle {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 4;
+        int peri = 2 * ( a + b); 
+        System.out.println("Perimeter of Rectangle = " + peri);
+    }
+}
