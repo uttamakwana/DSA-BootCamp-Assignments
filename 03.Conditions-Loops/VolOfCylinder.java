f60da856b189330c3cbc20667b7f6c29916fc286
Uttam Makwana <uttamakwana4503@gmail.com>
2023-02-13 17:31:07 +0530

    Volume of Cylinder

---
+++ 03.Conditions-Loops/VolOfCylinder.java
@@ -0,0 +1,8 @@
+public class VolOfCylinder {
+    public static void main(String[] args) {
+        float r = 3.3f;
+        float h = 2f;
+        float vol = 3.14f * r * r * h;
+        System.out.println("Volume of Cylinder = " + vol);
+    }
+}
