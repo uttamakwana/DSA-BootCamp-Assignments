7fb7875b1d1ddbed327552e51b597caff64468e3
Uttam Makwana <uttamakwana4503@gmail.com>
2023-02-13 17:10:54 +0530

    Program of Area

---
+++ 03.Conditions-Loops/AreaOfEqTriangle.java
@@ -0,0 +1,7 @@
+public class AreaOfEqTriangle {
+    public static void main(String[] args) {
+        float length = 3;
+        float area = 1.73205080757f * length * length / 4;
+        System.out.println("Area of Equilateral Triangle = " + area);
+    }
+}
---
+++ 03.Conditions-Loops/AreaOfRhombus.java
@@ -0,0 +1,9 @@
+public class AreaOfRhombus {
+    public static void main(String[] args) {
+        float d1 = 3;
+        float d2 = 3;
+        float area = d1 * d2 / 2;
+        
+        System.out.println("Area of Rhombus = " + area);
+    }
+}
