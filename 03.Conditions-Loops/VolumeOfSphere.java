4e81e45eec62ebac388db4ff30d3f1c6cca5d684
Uttam Makwana <uttamakwana4503@gmail.com>
2023-02-13 17:32:58 +0530

    Volume of Sphere

---
+++ 03.Conditions-Loops/VolumeOfSphere.java
@@ -0,0 +1,7 @@
+public class VolumeOfSphere {
+    public static void main(String[] args) {
+        float r = 3.3f;
+        float vol = (4f / 3f) * (3.14f) * r * r * r;
+        System.out.println("Volume of Sphere = " + vol);
+    }
+}
