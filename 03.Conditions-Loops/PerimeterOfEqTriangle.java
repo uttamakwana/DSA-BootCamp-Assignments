c314536c0e91fddeb05acb0c9585023953c13d26
Uttam Makwana <uttamakwana4503@gmail.com>
2023-02-13 17:19:46 +0530

    Perimeter of Equilateral Triangle

---
+++ 03.Conditions-Loops/PerimeterOfEqTriangle.java
@@ -0,0 +1,7 @@
+public class PerimeterOfEqTriangle {
+    public static void main(String[] args) {
+        int length = 3;
+        int peri = 3 * length;
+        System.out.println("Perimeter of Equilateral Triangle = " + peri);
+    }
+}
