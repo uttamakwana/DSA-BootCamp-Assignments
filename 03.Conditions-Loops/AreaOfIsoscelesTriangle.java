67fcb0601f0f15e20e31ca52a5a2fb4335c2c240
Uttam Makwana <uttamakwana4503@gmail.com>
2023-02-13 16:35:47 +0530

    Program of Area

---
+++ 03.Conditions-Loops/AreaOfIsoscelesTriangle.java
@@ -0,0 +1,8 @@
+public class AreaOfIsoscelesTriangle {
+    public static void main(String[] args) {
+        int base = 3;
+        int length = 2;
+        int area = base * length / 2;
+        System.out.println("The area of Isosceles Triangle = " + area);
+    }
+}
---
+++ 03.Conditions-Loops/AreaOfParallelogram.java
@@ -0,0 +1,8 @@
+public class AreaOfParallelogram {
+    public static void main(String[] args) {
+        int height = 3;
+        int base = 3;
+        int area = height * base;
+        System.out.println("Area of Parallelogram = " + area);
+    }
+}
