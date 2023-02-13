fe9e3d298444149667c67258cc72eeac9370bf41
Uttam Makwana <uttamakwana4503@gmail.com>
2023-02-13 18:59:46 +0530

    Program of Addition of two numbers

---
+++ 03.Conditions-Loops/AddtionOfTwoNumber.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class AddtionOfTwoNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter num 1 : ");
+        int number1 = input.nextInt();
+        System.out.print("Enter num 2 : ");
+        int number2 = input.nextInt();
+        System.out.println("The Addition of two number is = " + (number1 + number2));
+
+    }
+}
