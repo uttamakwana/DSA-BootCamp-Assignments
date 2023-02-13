c845b970f91c6395b89493d82f4dbc204cc0d51d
Uttam Makwana <uttamakwana4503@gmail.com>
2023-02-13 16:31:13 +0530

    Progarm of area of Rectangle

---
+++ 03.Conditions-Loops/AreaOfRectangle.java
@@ -0,0 +1,8 @@
+public class AreaOfRectangle {
+    public static void main(String[] args) {
+        int length = 3;
+        int width = 2;
+        int area = length * width;
+        System.out.println("The area of Rectangle = " + area);
+    }
+}
