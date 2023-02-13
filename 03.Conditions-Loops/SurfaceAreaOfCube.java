549e925d93cc132a2f5eb0ca6986c8e6a196f750
Uttam Makwana <uttamakwana4503@gmail.com>
2023-02-13 17:38:24 +0530

    Surface Area of Cube

---
+++ 03.Conditions-Loops/SurfaceAreaOfCube.java
@@ -0,0 +1,7 @@
+public class SurfaceAreaOfCube {
+    public static void main(String[] args) {
+        float a = 3f;
+        float ans = 6f * a * a;
+        System.out.println("Surface Area of Cube = " + ans);
+    }
+}
