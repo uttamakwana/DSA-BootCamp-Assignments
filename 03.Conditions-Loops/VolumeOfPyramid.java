c6887c40fe8d8b13ae90a653ec5b7fbf4dc0c681
Uttam Makwana <uttamakwana4503@gmail.com>
2023-02-13 17:34:31 +0530

    Volume of Pyramid

---
+++ 03.Conditions-Loops/VolumeOfPyramid.java
@@ -0,0 +1,9 @@
+public class VolumeOfPyramid {
+    public static void main(String[] args) {
+        float l = 3.2f;
+        float w = 2f;
+        float h = 3f;
+        float vol = l * w * h / 3f;
+        System.out.println("Volume of Pyramid = " + vol);
+    }
+}
