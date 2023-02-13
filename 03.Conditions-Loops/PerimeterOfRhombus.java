b8caa65833cb0080746fef35308b9badc18a8d69
Uttam Makwana <uttamakwana4503@gmail.com>
2023-02-13 17:24:29 +0530

    Perimeter of Rhombus

---
+++ 03.Conditions-Loops/PerimeterOfRhombus.java
@@ -0,0 +1,7 @@
+public class PerimeterOfRhombus {
+    public static void main(String[] args) {
+        int a = 3;
+        int peri = 4 * a;
+        System.out.println("Perimeter of Rhombus = " + peri);
+    }
+}
