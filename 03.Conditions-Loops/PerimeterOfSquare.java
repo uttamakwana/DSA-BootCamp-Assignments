0ee6e3095894269aff7cd31c6d5e7bed797faf18
Uttam Makwana <uttamakwana4503@gmail.com>
2023-02-13 17:23:16 +0530

    Perimeter of Square

---
+++ 03.Conditions-Loops/PerimeterOfSquare.java
@@ -0,0 +1,7 @@
+public class PerimeterOfSquare {
+    public static void main(String[] args) {
+        int a = 3;
+        int peri = 4 * a;
+        System.out.println("Perimeter of Square = " + peri);
+    }
+}
