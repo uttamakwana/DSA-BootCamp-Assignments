4c1129304e2e49a588fc872d9132eec72ca2cc75
Uttam Makwana <uttamakwana4503@gmail.com>
2023-02-13 17:36:28 +0530

    Area of Curved Surface of Cirlce

---
+++ 03.Conditions-Loops/CurvedSurfaceAreaOfCircle.java
@@ -0,0 +1,8 @@
+public class CurvedSurfaceAreaOfCircle {
+    public static void main(String[] args) {
+        float r = 2f;
+        float h = 2f;
+        float ans = 2f * (22f/ 7f) * r * h;
+        System.out.println("Area of Curved Surface of Cirlce = " + ans);
+    }
+}
