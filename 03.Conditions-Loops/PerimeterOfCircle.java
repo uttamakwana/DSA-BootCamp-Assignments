a94c6ffd3f550970c54326efc095138f81af761f
Uttam Makwana <uttamakwana4503@gmail.com>
2023-02-13 17:18:34 +0530

    Perimeter of Circle

---
+++ 03.Conditions-Loops/PerimeterOfCircle.java
@@ -0,0 +1,7 @@
+public class PerimeterOfCircle{
+    public static void main(String[] args) {
+        float radius = 7;
+        float peri = 2 * (3.14f)  * radius;
+        System.out.println("Perimeter of Circle = " + peri);
+    }
+}
