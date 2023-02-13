0c6bf072586a9632479482de2a1558de00fed54f
Uttam Makwana <uttamakwana4503@gmail.com>
2023-02-13 17:27:12 +0530

    Volume of Cone

---
+++ 03.Conditions-Loops/VolOfCone.java
@@ -0,0 +1,8 @@
+public class VolOfCone {
+    public static void main(String[] args) {
+        float r = 3f;
+        float h = 2.3f;
+        float vol = (((3.14f) * r * r * h) / 3f);
+        System.out.println("Volume of Cone = " + vol);
+    }
+}
