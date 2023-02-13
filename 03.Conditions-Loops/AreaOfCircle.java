b7c96a23bcd61b7e17191b34d9d9f32dd0a57c2c
Uttam Makwana <uttamakwana4503@gmail.com>
2023-02-13 16:25:44 +0530

    Program that gives area of circle

---
+++ 03.Conditions-Loops/AreaOfCircle.java
@@ -0,0 +1,7 @@
+public class AreaOfCircle {
+    public static void main(String[] args) {
+        int radius = 3;
+        int area = (22 / 7) * radius * radius; 
+        System.out.println("Area of Circle = " + area);
+    }
+}
