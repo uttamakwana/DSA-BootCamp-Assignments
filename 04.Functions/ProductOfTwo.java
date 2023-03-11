bea71e8cc3e728e5141ae1b7383ec88ad40fffc7
Uttam Makwana <uttamakwana4503@gmail.com>
2023-03-11 15:22:31 +0530

    Made a Program of product of two numbers

---
+++ 04.Functions/ProductOfTwo.java
@@ -0,0 +1,11 @@
+public class ProductOfTwo {
+    public static void main(String[] args){
+        int num1 = 2;
+        int num2 = 3;
+        product(num1, num2);
+    }
+
+    static void product(int a, int b){
+        System.out.println("The product = " + (a * b));
+    }
+}
