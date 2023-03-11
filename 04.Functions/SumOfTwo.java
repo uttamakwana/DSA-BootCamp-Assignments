99c6044cd12b68687b3d3021d56f08d50e5d3cc8
Uttam Makwana <uttamakwana4503@gmail.com>
2023-03-11 15:21:16 +0530

    Made a Program of sum of Two Numbers

---
+++ 04.Functions/SumOfTwo.java
@@ -0,0 +1,11 @@
+public class SumOfTwo {
+    public static void main(String[] args){
+        int num1 = 23;
+        int num2 = 32;
+        sum(num1, num2);
+    }
+
+    static void sum(int a, int b){
+        System.out.println("The sum = " + (a + b));
+    }
+}
